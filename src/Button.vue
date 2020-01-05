138b84a2e9133019156dd6f368376267f83e57fe
Jiawei <liujiaweidreamer@gmail.com>
2020-01-05 22:31:23 +0800

    add CSS for Button.vue

--- src/Button.vue
+++ src/Button.vue
@@ -1,8 +1,27 @@
 <template>
-  <button><slot>button</slot></button>
+  <button class="er-button"><slot>button</slot></button>
 </template>
 <script>
   export default {
 
   }
 </script>
+<style lang="scss">
+  .er-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
