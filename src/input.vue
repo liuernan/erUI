200e8407b1edf4aecfae4a5685b459daf24a5e78
Jiawei <liujiaweidreamer@gmail.com>
2020-05-01 23:22:28 +0800

    add CSS for input.vue, not done yet.

--- src/input.vue
+++ src/input.vue
@@ -1,5 +1,7 @@
 <template>
-  <input type="text" />
+  <div class="warpper">
+    <input type="text"/>
+  </div>
 </template>
 <script>
   export default {
@@ -7,5 +9,27 @@
   }
 </script>
 <style lang="scss" scoped>
+  $height: 30px;
+  $border-color: #999;
+  $border-color-hover: #666;
+  $box-shadow-color: rgba(0, 0, 0, .5);
 
+  .warpper {
+    > input {
+      height: $height;
+      border: 1px solid $border-color;
+      font-size: inherit;
+      padding: 0 8px;
+      border-radius: 4px;
+
+      &:hover {
+        border-color: $border-color-hover;
+      }
+
+      &:focus {
+        outline: none;
+        box-shadow: inset 0 1px 2px $box-shadow-color;
+      }
+    }
+  }
 </style>
