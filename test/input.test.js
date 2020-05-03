654f30e2dc7e83227e26aa369032daf29131faa3
Jiawei <liujiaweidreamer@gmail.com>
2020-05-03 23:11:39 +0800

    input.vue 的 props 的单元测试

--- test/input.test.js
+++ test/input.test.js
@@ -11,17 +11,75 @@ describe('Input', () => {
     expect(Input).to.ok;
   });
 
-  it('有属性 value', () => {
+  describe('的 props 接收', () => {
     const Constructor = Vue.extend(Input);
-    const vm = new Constructor({
-      propsData: {
-        value: '我是文本'
-      }
-    }).$mount();
+    let vm;
 
-    const inputElement = vm.$el.querySelector('input');
+    afterEach(() => {
+      vm.$destroy();
+    });
 
-    expect(inputElement.value).to.equals('我是文本');
-    vm.$destroy();
+    it('value', () => {
+      vm = new Constructor({
+        propsData: {
+          value: '我是文本'
+        }
+      }).$mount();
+
+      const $input = vm.$el.querySelector('input');
+      expect($input.value).to.equals('我是文本');
+    });
+
+    it('disabled', () => {
+      vm = new Constructor({
+        propsData: {
+          disabled: true
+        }
+      }).$mount();
+
+      const $input = vm.$el.querySelector('input');
+      expect($input.disabled).to.equals(true);
+    });
+
+    it('readonly', () => {
+      vm = new Constructor({
+        propsData: {
+          readonly: true
+        }
+      }).$mount();
+
+      const $input = vm.$el.querySelector('input');
+      expect($input.readOnly).to.equals(true);
+    });
+
+    it('error', () => {
+      vm = new Constructor({
+        propsData: {
+          error: '我是一条错误信息'
+        }
+      }).$mount();
+
+      const $use = vm.$el.querySelector('use');
+      expect($use.getAttribute('xlink:href')).to.equal('#icon-error');
+
+      const $msg = vm.$el.querySelector('.error-message');
+      expect($msg.innerText).to.equals('我是一条错误信息');
+
+    });
+
+    it('tips', () => {
+      vm = new Constructor({
+        propsData: {
+          tips: '我是一条提示信息'
+        }
+      }).$mount();
+
+      const $use = vm.$el.querySelector('use');
+      expect($use.getAttribute('xlink:href')).to.equal('#icon-tips');
+
+      const $msg = vm.$el.querySelector('.tips-message');
+      expect($msg.innerText).to.equals('我是一条提示信息');
+    });
   });
+
 });
