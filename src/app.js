72c741a6770530cbd569d5a9f572da15582bb2ac
Jiawei <liujiaweidreamer@gmail.com>
2020-05-06 15:02:32 +0800

    网格布局实现最基本功能：根据 er-row 中 er-col 的数量（大于等于 2 个），自动平均分配每个 col 的宽度。用 flex 布局轻松搞定。

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import Vue from "vue";
 import Button from "./button.vue";
 import ButtonGroup from "./button-group";
 import Icon from "./icon.vue";
-import Input from "./input"
+import Input from "./input";
+import Row from "./row";
+import Col from "./col";
 
 Vue.component("er-button", Button);
 Vue.component("er-icon", Icon);
 Vue.component("er-button-group", ButtonGroup);
 Vue.component("er-input", Input);
+Vue.component("er-row", Row);
+Vue.component("er-col", Col);
 
 new Vue({
   el: "#app",
---
+++ src/col.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col">
+    <slot></slot>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'erCol'
+  }
+</script>
+<style lang="scss" scoped>
+.col{
+  height: 100px;
+  width: 50%;
+  background-color: pink;
+  border: 1px dotted grey;
+
+}
+</style>
