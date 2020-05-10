f98455ece1981edc099852d92c40b7c63035233d
Jiawei <liujiaweidreamer@gmail.com>
2020-05-10 15:25:42 +0800

    layout 基本完成

--- src/layout.vue
+++ src/layout.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="layout">
+  <div class="layout" :class="classList">
     <slot></slot>
   </div>
 </template>
-<script></script>
+<script>
+  export default {
+    name: 'ErLayout',
+    data() {
+      return {
+        classList: {
+          hasSider: false
+        }
+      }
+    },
+    mounted() {
+      this.$children.forEach((vm) => {
+        if ('ErSider' === vm.$options.name) {
+          this.classList.hasSider = true;
+        }
+      });
+    }
+  }
+</script>
 <style lang="scss" scoped>
   .layout {
     display: flex;
     flex-direction: column;
+    flex-grow: 1;
+
+    &.hasSider {
+      flex-direction: row;
+    }
   }
 </style>
