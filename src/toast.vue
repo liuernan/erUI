c8df1ca25cf80224c2733b1a64d93bad52f1d75c
Jiawei <liujiaweidreamer@gmail.com>
2020-05-11 20:11:06 +0800

    写 toast 的基本样式

--- src/toast.vue
+++ src/toast.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="toast">
     <slot></slot>
   </div>
 </template>
@@ -9,5 +9,21 @@
   }
 </script>
 <style lang="scss" scoped>
-
+  $font-size: 14px;
+  $toast-min-height: 40px;
+  $toast-bg: rgba(0, 0, 0, 0.75);
+  .toast {
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: $font-size;
+    min-height: $toast-min-height;
+    background: $toast-bg;
+    color: white;
+    padding: 4px 16px;
+    border-radius: 4px;
+  }
 </style>
