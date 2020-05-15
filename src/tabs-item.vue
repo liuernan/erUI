da87f95251293ff40b002befef568f8e0040e4ec
Jiawei <liujiaweidreamer@gmail.com>
2020-05-15 22:47:04 +0800

    tabs 支持 disabled

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -26,7 +26,8 @@
     computed: {
       classList() {
         return {
-          'tabs-item-active': this.active
+          'tabs-item-active': this.active,
+          'tabs-item-disabled': this.disabled
         }
       }
     },
@@ -38,6 +39,7 @@
     },
     methods: {
       clickItem() {
+        if (this.disabled) return false;
         this.eventHub.$emit('update:selected', this.name);
       }
     }
@@ -45,6 +47,8 @@
 </script>
 <style lang="scss" scoped>
   $tabs-item-height: 40px;
+  $tabs-item-active-bg: lightpink;
+  $tabs-item-disabled-bg: lightgray;
   .tabs-item {
     padding: 0 1em;
     height: $tabs-item-height;
@@ -58,7 +62,12 @@
     }
 
     &.tabs-item-active {
-      background-color: pink;
+      background-color: $tabs-item-active-bg;
+    }
+
+    &.tabs-item-disabled {
+      background: $tabs-item-disabled-bg;
+      cursor: not-allowed;
     }
   }
 </style>
