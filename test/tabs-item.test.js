cd28f55f53a0cae5267bda4f1340ed8caa567d2b
Jiawei <liujiaweidreamer@gmail.com>
2020-05-16 15:47:30 +0800

    添加 tabs 组件的测试用例

--- test/tabs-item.test.js
+++ test/tabs-item.test.js
@@ -1,12 +1,60 @@
 const expect = chai.expect;
+
 import Vue from 'vue';
-import TabsItem from '../src/tabs-item';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
 
+import Tabs from '../src/tabs';
+import TabsNav from '../src/tabs-nav';
+import TabsItem from '../src/tabs-item';
+import TabsContent from '../src/tabs-content';
+import TabsPanel from '../src/tabs-panel';
+
+Vue.component('er-tabs', Tabs);
+Vue.component('er-tabs-nav', TabsNav);
+Vue.component('er-tabs-item', TabsItem);
+Vue.component('er-tabs-content', TabsContent);
+Vue.component('er-tabs-panel', TabsPanel);
+
 describe('TabsItem', () => {
   it('存在.', () => {
     expect(TabsItem).to.be.exist;
-  })
+  });
+
+  describe('的 props 接收', () => {
+
+    const Constructor = Vue.extend(TabsItem);
+
+    it('name', function () {
+      const vm = new Constructor({
+        propsData: {
+          name: 'xxx'
+        }
+      }).$mount();
+
+      expect(vm.$el.getAttribute('data-name')).to.eq('xxx');
+
+      vm.$el.remove();
+      vm.$destroy();
+    });
+
+    it('disabled', function () {
+      const vm = new Constructor({
+        propsData: {
+          disabled: true
+        }
+      }).$mount();
+
+      expect(vm.$el.classList.contains('tabs-item-disabled')).to.be.true;
+
+      const spy = sinon.fake();
+      vm.$on('click', spy);
+      vm.$el.click();
+      expect(spy).to.be.callCount(0);
+
+      vm.$el.remove();
+      vm.$destroy();
+    });
+  });
 });
--- test/tabs.test.js
+++ test/tabs.test.js
@@ -1,12 +1,72 @@
 const expect = chai.expect;
+
 import Vue from 'vue';
-import Tabs from '../src/tabs';
 
 Vue.config.productionTip = false;
 Vue.config.devtools = false;
 
+import Tabs from '../src/tabs';
+import TabsNav from '../src/tabs-nav';
+import TabsItem from '../src/tabs-item';
+import TabsContent from '../src/tabs-content';
+import TabsPanel from '../src/tabs-panel';
+Vue.component('er-tabs', Tabs);
+Vue.component('er-tabs-nav', TabsNav);
+Vue.component('er-tabs-item', TabsItem);
+Vue.component('er-tabs-content', TabsContent);
+Vue.component('er-tabs-panel', TabsPanel);
+
 describe('Tabs', () => {
   it('存在.', () => {
     expect(Tabs).to.be.exist;
-  })
+  });
+
+  describe('的 props 接收', () => {
+    it('selected', (done) => {
+      const div = document.createElement('div');
+      div.innerHTML = `    
+        <er-tabs :selected.sync="selectedTab" direction="horizontal">
+          <er-tabs-nav>
+            <template slot="er-nav-button">
+              <button icon="settings">操作按钮</button>
+            </template>
+            <er-tabs-item class="item-test" name="women">美女</er-tabs-item>
+            <er-tabs-item class="item-test" name="live">直播</er-tabs-item>
+            <er-tabs-item class="item-test" name="videos" disabled>小电影</er-tabs-item>
+          </er-tabs-nav>
+          <er-tabs-content>
+            <er-tabs-panel class="panel-test" name="women">美女的区域美女的区域美女的区域美女的区域美女的区域美女的区域</er-tabs-panel>
+            <er-tabs-panel class="panel-test" name="live">直播的区域直播的区域直播的区域直播的区域直播的区域直播的区域直播的区域</er-tabs-panel>
+            <er-tabs-panel class="panel-test" name="videos">全是小电影全是小电影全是小电影全是小电影全是小电影全是小电影全是小电影全是小电影全是小电影</er-tabs-panel>
+          </er-tabs-content>
+        </er-tabs>
+      `;
+      const vm = new Vue({
+        el: div,
+        data: {
+          selectedTab: 'live'
+        }
+      });
+
+      vm.$nextTick(() => {
+        const items = vm.$el.querySelectorAll('.tabs-item');
+        const panels = vm.$el.querySelectorAll('.tabs-panel');
+
+        items.forEach((item)=>{
+          if ('live' === item.getAttribute('data-name')) {
+            expect(item.classList.contains('tabs-item-active')).to.eq(true);
+          } else {
+            expect(item.classList.contains('tabs-item-active')).to.eq(false);
+          }
+        });
+
+        expect(panels.length).to.eq(1);
+        expect(panels[0].getAttribute('data-name')).to.eq('live');
+
+        vm.$el.remove();
+        vm.$destroy();
+        done();
+      });
+    });
+  });
 });
