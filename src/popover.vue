25919c1d9f1af6e9cc09646b41a0582021695322
Jiawei <liujiaweidreamer@gmail.com>
2020-05-17 15:04:21 +0800

    popover 实现基本功能：点击按钮出现，点击按钮消失

--- src/popover.vue
+++ src/popover.vue
@@ -1,16 +1,39 @@
 <template>
-  <div>
-    <slot name="popover-content"></slot>
-    <slot></slot>
+  <div class="popover">
+    <div class="popover-content-warpper" v-if="visible">
+      <slot name="popover-content"></slot>
+    </div>
+    <div @click="clickButton">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "ErPopover"
+    name: "ErPopover",
+    data() {
+      return {
+        visible: false
+      }
+    },
+    methods: {
+      clickButton() {
+        this.visible = !this.visible;
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  .popover {
+    display: inline-block;
+    position: relative;
 
+    > .popover-content-warpper {
+      position: absolute;
+      bottom: 110%;
+      border: 1px solid gray;
+    }
+  }
 </style>
