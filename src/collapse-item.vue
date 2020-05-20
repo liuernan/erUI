fc28c871ec9d357bc4a832c3ff42e3d647e486b3
Jiawei <liujiaweidreamer@gmail.com>
2020-05-20 18:42:57 +0800

    开始写 collapse

---
+++ src/collapse-item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ErCollapseItem"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/collapse.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ErCollapse"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
