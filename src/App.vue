734b97a9266638caa33f8cbcb57bd78d375fd0fa
Jiawei <liujiaweidreamer@gmail.com>
2020-06-05 13:36:31 +0800

    update docs

--- src/App.vue
+++ src/App.vue
@@ -222,19 +222,7 @@
         <er-col :span="4">6</er-col>
       </er-row>
     </div>
-    <div class="buxiangqimingzi">
-      <er-button icon="upload" :loading=loading1 @click="loading1 = !loading1">上传</er-button>
-      <er-button icon="download" icon-position="right" :loading=loading2 @click="loading2 = !loading2">下载</er-button>
-    </div>
-    <div class="buxiangqimingzi">
-      <er-button icon="left" icon-position="left">上一步</er-button>
-      <er-button icon="right" icon-position="right">下一步</er-button>
-    </div>
-    <div class="buxiangqimingzi">
-      <er-button icon="settings">设置</er-button>
-      <er-button icon="left" icon-position="left">上一步</er-button>
-      <er-button icon="right" icon-position="right">下一步</er-button>
-    </div>
+
 
     <div class="buxiangqimingzi">
       <input type="text">
