d452300002e7f339b967182480cab7e0a08a97cf
Jiawei <liujiaweidreamer@gmail.com>
2020-06-10 13:44:23 +0800

    update tabs-item.spec.js

--- tests/unit/tabs-item.spec.js
+++ tests/unit/tabs-item.spec.js
@@ -1,4 +1,10 @@
-const expect = chai.expect;
+import chai, {expect} from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+
+chai.use(sinonChai)
+
+import {shallowMount, mount} from '@vue/test-utils'
 
 import Vue from 'vue';
 
