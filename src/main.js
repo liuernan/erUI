58707b18449b3e7f6269314bfdc34dfea998a689
Jiawei <liujiaweidreamer@gmail.com>
2020-06-16 19:51:28 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -3,51 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-import Button from './components/button/button.vue';
-import ButtonGroup from './components/button/button-group';
-import Icon from './components/icon/icon.vue';
-import Input from './components/input';
-import Row from './components/grid/row';
-import Col from './components/grid/col';
-import Layout from './components/layout/layout';
-import Header from './components/layout/header';
-import Sider from './components/layout/sider';
-import Content from './components/layout/content';
-import Footer from './components/layout/footer';
-import Toast from './components/toast';
-import Plugin from './components/plugin';
-import Tabs from './components/tabs/tabs';
-import TabsNav from './components/tabs/tabs-nav';
-import TabsItem from './components/tabs/tabs-item';
-import TabsContent from './components/tabs/tabs-content';
-import TabsPanel from './components/tabs/tabs-panel';
-import Popover from './components/popover';
-import Collapse from './components/collapse/collapse';
-import CollapseItem from './components/collapse/collapse-item';
-import Cascader from './components/cascader/cascader';
-
-Vue.component('er-button', Button);
-Vue.component('er-icon', Icon);
-Vue.component('er-button-group', ButtonGroup);
-Vue.component('er-input', Input);
-Vue.component('er-row', Row);
-Vue.component('er-col', Col);
-Vue.component('er-layout', Layout);
-Vue.component('er-header', Header);
-Vue.component('er-sider', Sider);
-Vue.component('er-content', Content);
-Vue.component('er-footer', Footer);
-Vue.component('er-toast', Toast);
-Vue.component('er-tabs', Tabs);
-Vue.component('er-tabs-nav', TabsNav);
-Vue.component('er-tabs-item', TabsItem);
-Vue.component('er-tabs-content', TabsContent);
-Vue.component('er-tabs-panel', TabsPanel);
-Vue.component('er-popover', Popover);
-Vue.component('er-collapse', Collapse);
-Vue.component('er-collapse-item', CollapseItem);
-Vue.component('er-cascader', Cascader);
-
 Vue.use(Plugin);
 
 new Vue({
