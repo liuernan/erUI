70656ddb4e9045cad82eec2f9803932159b6f338
Jiawei <liujiaweidreamer@gmail.com>
2020-07-11 21:07:43 +0800

    多级菜单的 API

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,32 @@
         <er-nav-item name="contact">联系我们</er-nav-item>
       </er-nav>
     </div>
+    <div class="app-wrapper">
+      <er-nav :selected.sync="selected4">
+        <er-nav-item name="home">首页</er-nav-item>
+        <er-sub-nav>
+          <template slot="title">产品</template>
+          <er-nav-item name="products1">产品1</er-nav-item>
+          <er-nav-item name="products2">产品2</er-nav-item>
+          <er-nav-item name="products3">产品3</er-nav-item>
+        </er-sub-nav>
+        <er-nav-item name="about">关于我们</er-nav-item>
+        <er-sub-nav>
+          <template slot="title">联系我们</template>
+          <er-sub-nav>
+            <template slot="title">网络</template>
+            <er-nav-item name="qq">QQ</er-nav-item>
+            <er-nav-item name="wechat">微信</er-nav-item>
+            <er-nav-item name="weibo">微博</er-nav-item>
+          </er-sub-nav>
+          <er-sub-nav>
+            <template slot="title">电话</template>
+            <er-nav-item name="tel">座机</er-nav-item>
+            <er-nav-item name="phone">手机</er-nav-item>
+          </er-sub-nav>
+        </er-sub-nav>
+      </er-nav>
+    </div>
   </div>
 </template>
 
@@ -47,6 +73,7 @@
         selected: ['home'],
         selected2: ['products1', 'products2', 'products3'],
         selected3: ['contact'],
+        selected4: ['about']
       }
     },
     methods: {}
